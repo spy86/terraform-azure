36750d233016c0a33efbab1f9b81a2f9991de66d
spy86 <test@localhost>
2021-12-06 18:38:50 +0100

    Update TF for Azure Linux VM

--- azure_linux_vm/backend.tf
+++ azure_linux_vm/backend.tf
@@ -1,8 +1,15 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "weu-common-rg"
-    storage_account_name = "tfstated"
-    container_name       = "tfstate"
-    key                  = "${var.environment}-${var.compute_name}-${var.region}-vm.terraform.tfstate"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.88.1"
+    }
   }
+  backend "azurerm" {
+        resource_group_name = "weu-common-rg"
+        storage_account_name = "tfstated"
+        container_name = "tfstate"
+        key = "linux-vm-terraform.tfstate"
+  }
+required_version = ">= 0.15"
 }
